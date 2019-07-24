549bcf01c2206db732aab5a4e13e9f7c66964f25
M. Irfan Hidayat <irfanhidayatms@outlook.com>
2019-07-24 13:01:42 +0700

    initial commit irufano

---
+++ GeolocationExample/GeolocationExample/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace GeolocationExample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        } 
+
+
+
+        private async void ButtonLocation_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+                
+                if (location != null)
+                { 
+                    labelLatitude.Text = "Latitude: " + location.Latitude.ToString();
+                    labelLongitude.Text = "Longitude:" + location.Longitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Failed", fnsEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Failed", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Failed", ex.Message, "OK");
+            }
+        }
+    }
+}
